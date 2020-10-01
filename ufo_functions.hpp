ac4b84236d6885e30067a06eada2c853dd639ed8
Irina Nastase <nastase.irina90@yahoo.com>
2020-10-01 12:29:27 +0300

    Create ufo_functions.hpp

---
+++ ufo_functions.hpp
@@ -0,0 +1,8 @@
+#include <vector>
+#include <string>
+void greet();
+void end_game(std::string codeword, std::string answer);
+void display_misses(int misses);
+void display_status(std::string incorrect, std::string answer);
+
+// Declare functions
